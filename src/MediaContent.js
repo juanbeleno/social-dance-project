77ab9644c1ec8d6a97771a35c56b3e4f7ef4e851
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2022-12-21 15:51:25 -0500

    Change PlayBack Rate

--- src/MediaContent.js
+++ src/MediaContent.js
@@ -6,8 +6,14 @@ export default function MediaContent(props) {
         const {filepath, orientation} = props;
 
         if (filepath.includes(".mp4")) {
+            const videoRef= useRef();
+            const setPlayBack = () => {
+                videoRef.current.playbackRate = 0.75;
+            };
             return (
                 <video autoPlay loop
+                    ref={videoRef}
+                    onCanPlay={() => setPlayBack()}
                     className={`m-auto ${orientation === 'portrait' ? 'h-full' : 'w-full'}`}>
                     <source src={`img/${filepath}`} type="video/mp4"/>
                 Your browser does not support the video tag.
